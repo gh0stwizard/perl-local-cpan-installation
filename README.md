ab7910239ffdc0f99465d95d765f4fb8d9972077
gh0stwizard <vitaliy.tokarev@gmail.com>
2015-01-14 23:02:45 +0300

    update README

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# perl-local-cpan-installation
-All-in-one script to perform install modules from CPAN for an ordinary user
+Description
+===========
+
+All-in-one script to perform install modules from CPAN for an ordinary user.
+
+This is shell scipt and applies for any *nix-like system:
+
+* GNU/Linux
+* FreeBSD
+* OpenBSD
+
+
+Usage
+==========
+
+```
+shell> sh user_cpan_installation.sh
+```
+
+* Answer on questions in CPAN shell
+* Exit from CPAN shell by typing "q" without quotes
+* Use PERL5LIB environment variable in your ~/.bashrc, ~/.zshrc, etc (just add export PERL5LIB="...")
+
+That's all. Install your modules as usual via CPAN shell:
+
+```
+shell> perl -MCPAN -e shell
+```
+
+Customization
+=============
+
+Look at *user_cpan_installation.sh* script via your editor. By default:
+
+* CPAN's configuration files lies in $HOME/.cpan
+* Installed modules are place in $HOME/perl
--- user_cpan_installation.sh
+++ user_cpan_installation.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# apply next settings for Perl
+PERL_FILE="/usr/bin/perl"
+
 # cpan defaults
 CPAN_HOME="$HOME/.cpan"
 CPAN_CONF="${CPAN_HOME}/CPAN/MyConfig.pm"
@@ -7,6 +10,7 @@ CPAN_CONF="${CPAN_HOME}/CPAN/MyConfig.pm"
 # where is perl files will be kept
 PERL_HOME="$HOME/perl"
 
+# our magic starts here
 mkdir -p ${CPAN_HOME}/CPAN || exit 1
 
 echo "\$CPAN::Config->{cpan_home}='${CPAN_HOME}';\n\
@@ -16,14 +20,14 @@ echo "\$CPAN::Config->{cpan_home}='${CPAN_HOME}';\n\
 \$CPAN::Config->{mbuild_install_arg}='--installdirs=site --install_base=${PERL_HOME}';\
 " > ${CPAN_CONF}
 
-# initialize cpan
-perl -MCPAN -e shell
+# initialize cpan to fill in required fields into MyConfig.pm
+${PERL_FILE} -MCPAN -e shell
 
-ARCH_NAME=`perl -MConfig -e 'print $Config{archname}'`
+# auto generate PERL5LIB environment variable
+ARCH_NAME=`${PERL_FILE} -MConfig -e 'print $Config{archname}'`
 PERL5LIB="${PERL_HOME}/lib/perl5:${PERL_HOME}/lib/perl5/${ARCH_NAME}"
 
 echo
 echo "Export the variable below in your ~/.bashrc or ~/.zshrc file:"
 echo "   PERL5LIB=${PERL5LIB}"
 echo
-
